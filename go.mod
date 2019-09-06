69f7a86a49c50b0074dd32e286fdfebbdb4a13ff
Matt Conway <mattconway55@gmail.com>
2019-09-06 05:59:49 -0400

    added xorm ORM functionality, updated proto submodule, added documentation

--- go.mod
+++ go.mod
@@ -3,31 +3,16 @@ module github.com/kari-labs/url-dev-api-urls
 go 1.12
 
 require (
-	github.com/go-sql-driver/mysql v1.4.1 // indirect
-	github.com/gogo/protobuf v1.3.0 // indirect
-	github.com/golang/protobuf v1.3.1
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/go-xorm/xorm v0.7.6
+	github.com/golang/protobuf v1.3.2
 	github.com/google/uuid v1.1.1
-	github.com/gorilla/websocket v1.4.1 // indirect
-	github.com/graphql-go/graphql v0.7.8 // indirect
 	github.com/joho/godotenv v1.3.0
-	github.com/juju/errors v0.0.0-20190806202954-0232dcc7464d // indirect
 	github.com/kari-labs/config v0.0.0-20190902212622-6d4ada69cb78
-	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/matoous/go-nanoid v1.1.0
-	github.com/ngaut/log v0.0.0-20180314031856-b8e36e7ba5ac // indirect
-	github.com/pingcap/check v0.0.0-20190102082844-67f458068fc8 // indirect
-	github.com/pingcap/errors v0.11.0 // indirect
-	github.com/pingcap/parser v0.0.0-20190506092653-e336082eb825 // indirect
-	github.com/pingcap/tipb v0.0.0-20190428032612-535e1abaa330 // indirect
-	github.com/rakyll/statik v0.1.6 // indirect
-	github.com/samsarahq/go v0.0.0-20190516185622-fbd7434ad75e // indirect
-	github.com/samsarahq/thunder v0.5.0
-	github.com/satori/go.uuid v1.2.0 // indirect
-	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
-	github.com/siddontang/go v0.0.0-20180604090527-bdc77568d726 // indirect
-	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07 // indirect
-	github.com/siddontang/go-mysql v0.0.0-20170918131730-4ed9ef1d3614 // indirect
-	github.com/sirupsen/logrus v1.4.1 // indirect
-	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/grpc v1.23.0
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	xorm.io/core v0.7.0
 )
+
+replace github.com/kari-labs/config => ../config
--- server.go
+++ server.go
@@ -8,77 +8,143 @@ import (
 	"log"
 	"net"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	"github.com/kari-labs/config"
 	"github.com/kari-labs/url-dev-api-urls/urlapi"
 	gonanoid "github.com/matoous/go-nanoid"
-	"github.com/samsarahq/thunder/livesql"
-	"github.com/samsarahq/thunder/sqlgen"
 	"google.golang.org/grpc"
+	"xorm.io/core"
 )
 
-type url struct {
-	UUID        uuid.UUID `sql:"uuid,primary" graphql:"id,key"`
-	OriginalURL string    `sql:"originalURL"`
-	ShortURL    string    `sql:"shortURL"`
-	Visits      int       `sql:"visits"`
+// publicURL represents a URL created by anonymous users
+type publicURL struct {
+	// (BUG: The UUID's are currently being stored with quotes around them in the database for some reason.
+	//	Probably has something to do with the way XORM converts them to 'varchar(255)')
+	UUID        uuid.UUID `xorm:"varchar(255) not null pk 'uuid'"`
+	OriginalURL string    `xorm:"'originalURL'"`
+	ShortURL    string    `xorm:"'shortURL'"`
+	Visits      int       `xorm:"'visits'"`
 }
 
+// TableName is a method used by Xorm to determine the correct SQL tablename to use when syncing the struct
+func (*publicURL) TableName() string {
+	return "public_urls"
+}
+
+// server contains a reference to our database connection as well as our gRPC methods
 type server struct {
-	db *livesql.LiveDB
+	db *xorm.Engine
 }
 
-func (s *server) Shorten(ctx context.Context, shreq *urlapi.ShortenRequest) (*urlapi.ShortenResponse, error) {
-	if shreq.Url == "" {
+// Shorten takes a URL and RequestType of RETRIEVE or FORCE_GEN
+// Using the type of the request, the method either attempts to retrieve and existing
+// 		URL or just generates a new one
+func (s *server) Shorten(ctx context.Context, req *urlapi.ShortenRequest) (*urlapi.ShortenResponse, error) {
+	if req.GetUrl() == "" {
 		return nil, fmt.Errorf("Cannot shorten empty URL")
 	}
 
+	if req.GetType() == urlapi.ShortenRequest_RETRIEVE {
+		if has, err := s.db.Where("originalURL = ?", req.GetUrl()).Exist(&publicURL{}); err != nil {
+			return nil, fmt.Errorf("Error checking database for existing short URL: %v", err)
+		} else if has {
+			var temp publicURL
+			if _, err := s.db.Where("originalURL = ?", req.GetUrl()).Get(&temp); err != nil {
+				return nil, fmt.Errorf("Error retrieving existing short URL from database: %v", err)
+			}
+
+			return &urlapi.ShortenResponse{
+				Shorturl: temp.ShortURL,
+			}, nil
+		}
+	}
+
+	// URL generation happens in 2 cases
+	// 1. User attempts to create a shortened URL. The client passes in a Retrieve request by default, attempting to return
+	// 		an already existing short URL. A short URL doesn't already exist for this URL so we generate one.
+	// 		(TODO: Not sure if this should be default behavior. Could return message to client saying that a URL hasn't been created yet?)
+	// 2. User sends a request with type FORCE_GEN. This forces the server to generate a new short URL by default.
+
 	id, _ := gonanoid.Nanoid(9)
-	url := url{
+	temp := publicURL{
 		UUID:        uuid.New(),
-		OriginalURL: shreq.Url,
+		OriginalURL: req.Url,
 		ShortURL:    fmt.Sprintf("url.dev/%s", id),
 		Visits:      0,
 	}
-	if _, err := s.db.InsertRow(ctx, &url); err != nil {
-		return nil, err
+
+	// (BUG: We shoudn't need to specify the table here, XORM should use the TableName() method to resolve this.
+	//	Not sure why I have to manually specify.)
+	if _, err := s.db.Table("public_urls").Insert(temp); err != nil {
+		return nil, fmt.Errorf("Error inserting new short URL to database: %v", err)
 	}
-	return &urlapi.ShortenResponse{Shorturl: url.ShortURL}, nil
+	return &urlapi.ShortenResponse{Shorturl: temp.ShortURL}, nil
 }
 
+// Lengthen is a method which takes a short URL to be transformed back into its original form
+// The resolver uses this to redirect users to the correct locations
 func (s *server) Lengthen(ctx context.Context, lereq *urlapi.LengthenRequest) (*urlapi.LengthenResponse, error) {
 	if lereq.Url == "" {
 		return nil, fmt.Errorf("Cannot lengthen empty URL")
 	}
 
-	var url *url
+	var temp publicURL
 
-	if err := s.db.QueryRow(ctx, &url, sqlgen.Filter{"shortURL": lereq.Url}, nil); err != nil {
+	if _, err := s.db.Where("shortURL = ?", lereq.Url).Get(&temp); err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 
-	return &urlapi.LengthenResponse{Longurl: url.OriginalURL}, nil
+	return &urlapi.LengthenResponse{Longurl: temp.OriginalURL}, nil
 }
 
 func main() {
-	conf := config.New()
-	sqlgenSchema := sqlgen.NewSchema()
-	sqlgenSchema.MustRegisterType("public_urls", sqlgen.UniqueId, url{})
-
-	db, err := livesql.Open(conf.DB.Host, conf.DB.Port, conf.DB.User, conf.DB.Password, conf.DB.Database, sqlgenSchema)
+	// Load in environment variables for development
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		log.Fatal("Error loading .env file")
 	}
 
+	// Creating a new config for connection to DB
+	conf := config.New()
+
+	// Initialize connection
+	engine, err := xorm.NewEngine("mysql", conf.CreateConnectionString())
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+
+	// By default XORM uses 'SnakeMapper', which attempts to put underscores inbetween words.
+	// It does this by inserting an underscore before each capital letter. This causes problems with the acronym URL
+	//		and other words we use.
+	// GonicMapper just doesn't insert underscores between words, instead lowercasing them.
+	// See docs for more info - https://github.com/go-xorm/manual-en-US/blob/master/chapter-02/1.mapping.md
+	engine.SetTableMapper(core.GonicMapper{})
+
+	// Sync DB engine to use proper table for queries, inserts.
+	// (TODO: We should be using database sessions and having different structs for public_urls, org_urls, etc
+	// 		This current method isn't good because this service will ideally handle all URL interaction, not just
+	//		"public URL" creation)
+	err = engine.Sync2(new(publicURL))
+	if err != nil {
+		log.Fatalf("error syncing struct to database table: %v", err)
+	}
+
+	// Initialize server with DB connection
+	apiservice := server{
+		db: engine,
+	}
+
+	// Initialize a TCP listener for the gRPC server
 	lis, err := net.Listen("tcp", "0.0.0.0:3009")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	apiservice := server{
-		db: db,
-	}
-
+	// Initialize, register, and start gRPC server
 	grpcserver := grpc.NewServer()
 	urlapi.RegisterShortenAPIServiceServer(grpcserver, &apiservice)
 	if err := grpcserver.Serve(lis); err != nil {
